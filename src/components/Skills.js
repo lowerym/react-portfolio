daaebea0cee07f3e8ff85fec501bc6ed4fd165b9
Matthew Lowery <matthewdlowery@msn.com>
2024-05-13 19:45:27 -0500

    Built out About, Projects, and Skills components and worked on data

---
+++ src/components/Skills.js
@@ -0,0 +1,11 @@
+import { BadgeCheckIcon, ChipIcon } from "@heroicons/react/solid";
+import React from "react";
+import { skills } from "../data";
+
+export default function Skills() {
+  return(
+    <section id="skills">
+      <div className="container px-5 py-10 mx-auto"></div>
+    </section>
+  )
+}
