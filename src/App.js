05193cd0dacad6415dacbdde21dec85e7882b785
Matthew Lowery <matthewdlowery@msn.com>
2024-05-14 23:36:54 -0500

    Fixed App.js

--- src/App.js
+++ src/App.js
@@ -2,17 +2,19 @@ import React from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import NavBar from "./components/NavBar";
-import Resume from "./components/Resume";
-import Portfolio from "./components/Portfolio";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <NavBar />
       <About />
-      <Portfolio />
+      <Projects />
+      <Skills />
+      <Testimonials />
       <Contact />
-      <Resume />
     </main>
   );
 }
