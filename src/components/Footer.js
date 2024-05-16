04c25f01c410bee19dd11269372029f97ed3524e
Matthew Lowery <matthewdlowery@msn.com>
2024-05-15 22:09:00 -0500

    Changed footer background color

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   let date = new Date();
   let year = date.getFullYear();
   return (
-<footer class="bg-white dark:bg-gray-900">
+<footer class="bg-gray-900">
     <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
       <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
       <div class="sm:flex sm:items-center sm:justify-between">
