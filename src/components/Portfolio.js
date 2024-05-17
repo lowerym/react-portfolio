e517d73e0b9dfd593a43a697dcf211adf4ecf83e
Matthew Lowery <matthewdlowery@msn.com>
2024-05-16 20:38:24 -0500

    Built out portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import code from '../assets/code2.png';
 
-const Works = () => {
+const Portfolio = () => {
   return (
-    <div name='work' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
+    <div name='portfolio' className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
       <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
         <div className='pb-8 w-full flex justify-center items-center flex-col'>
           <p className='text-4xl font-bold inline border-b-4 text-gray-300 border-cyan-500'>
-            Work
+            Portfolio
           </p>
           <p className='py-6 text-2xl'>Check out some of my most recent work</p>
         </div>
@@ -25,12 +24,12 @@ const Works = () => {
               </span>
               <p className='text-center'>A single page text editor built as a Progressive Web Application (PWA)</p>
               <div className='pt-8 text-center'>
-                <a href='https://text-editor-dqj0.onrender.com/'>
+                <a href='https://text-editor-dqj0.onrender.com/' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/lowerym/text-editor'>
+                <a href='https://github.com/lowerym/text-editor' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -49,12 +48,12 @@ const Works = () => {
               </span>
               <p className='text-center'>An API for a social network web application</p>
               <div className='pt-8 text-center'>
-                <a href='https://drive.google.com/file/d/1K5G5oStv6vsG0EjTCKoeCz3oBl3mq9zl/view?usp=sharing'>
+                <a href='https://drive.google.com/file/d/1K5G5oStv6vsG0EjTCKoeCz3oBl3mq9zl/view?usp=sharing' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='https://github.com/lowerym/social-network-api'>
+                <a href='https://github.com/lowerym/social-network-api' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -71,14 +70,14 @@ const Works = () => {
               <span className=' text-lg font-bold text-white tracking-wider'>
                 E-Commerce Back-End
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>The Back End of an E-Commerce Platform</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://drive.google.com/file/d/1licOZllqTeRiC9FC-oE6GPwm7whmZBo-/view?usp=drive_link' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/lowerym/ecommerce-back-end' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -87,22 +86,22 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(./assets/project-4.png)` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                Employee Tracker
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>A command-line application that manages a company's database</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://drive.google.com/file/d/15FVpAGdEdy41EiFNE1gkL95qUCSNfV4q/view' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/lowerym/employee-tracker' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -111,22 +110,22 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(./assets/project-5.png)` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                Note Taker
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>Application that allows users to write and save notes</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://note-taker-application-9qs0.onrender.com/' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/lowerym/note-taker' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -135,22 +134,22 @@ const Works = () => {
             </div>
           </div>
           <div
-            style={{ backgroundImage: `url(${code})` }}
+            style={{ backgroundImage: `url(./assets/project-6.png)` }}
             className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'
           >
             {/* Hover Effects */}
             <div className='opacity-0 group-hover:opacity-100 flex justify-center items-center flex-col'>
               <span className=' text-lg font-bold text-white tracking-wider'>
-                CBT Application
+                Movie Search Engine
               </span>
-              <p className='text-center'>A CBT web application built with React and Mongodb</p>
+              <p className='text-center'>Basic search engine designed to search through the AFI's Top 100 Movies of All Time</p>
               <div className='pt-8 text-center'>
-                <a href='/'>
+                <a href='https://lowerym.github.io/movie-search-engine/' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Demo
                   </button>
                 </a>
-                <a href='/'>
+                <a href='https://github.com/lowerym/movie-search-engine' target="_blank">
                   <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>
                     Code
                   </button>
@@ -164,4 +163,4 @@ const Works = () => {
     </div>
   );
 };
-export default Works;
+export default Portfolio;
